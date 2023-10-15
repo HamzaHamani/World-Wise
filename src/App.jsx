f032fd6f2858c4c479d9db58aafa2e19e7569e26
HamzaHamani <hamzahamani2004@gmail.com>
2023-10-15 20:09:44 +0100

    adding fake data and components

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
 
 function App() {
   return (
@@ -15,13 +16,14 @@ function App() {
           <Route path="pricing" element={<Pricing />} />
           <Route path="product" element={<Product />} />
           <Route path="app" element={<AppLayout />}>
-            <Route index element={<p>countries</p>} />
-            <Route path="cities" element={<p>cities</p>} />
+            {/* 👇 THIS ROUTE GONNA BE DEFUALT WE GONNA SE INSIDE APP WHERE WE PUT IT <OUTLER*/}
+            <Route index element={<CityList />} />
+            <Route path="cities" element={<CityList />} />
             <Route path="countries" element={<p>countries</p>} />
             <Route path="form" element={<p>form</p>} />
           </Route>
           <Route path="/login" element={<Login />} />
-
+          {/*! for invalid path URL */}
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
