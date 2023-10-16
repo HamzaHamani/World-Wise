34d6c7d2799da28123de216c71cdef5e4a89cef4
HamzaHamani <hamzahamani2004@gmail.com>
2023-10-16 21:44:40 +0100

    small changes for routing

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import styles from "./Login.module.css";
 import PageNav from "../components/PageNav";
+import { useParams } from "react-router-dom";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+  const x = useParams();
+  console.log(x);
 
   return (
     <main className={styles.login}>
