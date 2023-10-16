ce5a5ee42d851ab363fd1dc9e77f93e71719b275
HamzaHamani <hamzahamani2004@gmail.com>
2023-10-16 02:27:20 +0100

    added unique country lis

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -1,8 +1,9 @@
-import CityItem from "./CityItem";
 import styles from "./CountryList.module.css";
 import Spinner from "./Spinner";
 import Message from "./Message";
+import CountryItem from "./CountryItem";
 function CountryList({ loading, cities }) {
+  console.log(cities);
   const length = cities.length;
 
   if (loading) return <Spinner />;
@@ -10,10 +11,22 @@ function CountryList({ loading, cities }) {
     return (
       <Message message="Add your List of cities you visited by clicking on the map" />
     );
+  const countries = cities.reduce((arr, city) => {
+    if (arr.map((el) => el.country).includes(city.country)) {
+      return arr;
+    } else {
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    }
+  }, []);
+
+  // const countries = cities.reduce((acc, cur) => {
+  //   if (acc.map((el) => el.country).includes(cur.country)) return acc;
+  //   else return [...acc, { country: cur.country, emoji: cur.emoji }];
+  // }, []);
   return (
-    <ul className={styles.countriesList}>
-      {cities.map((city) => (
-        <CityItem city={city} key={city.id} />
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.country} />
       ))}
     </ul>
   );
